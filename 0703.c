91829178ff5d2fe2f9d4460a38cac930c940ea79
PrzKem <41024254+PrzKem@users.noreply.github.com>
2018-12-03 22:14:16 +0100

    Create 0703.c

---
+++ 0703.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  FILE *wp;
+  int N,pierwsza=0,poz,i=0;
+  int ilosc[10]={0};
+  double liczba,maksimum, minimum, przedzialCalkowity, krokPrzedzialu,poczatek,koniec;
+  wp = fopen("dane.txt","r");
+  scanf("%d",&N);
+  while(fscanf(wp,"%lf\n",&liczba) == 1)
+  {
+    if(pierwsza == 0)
+    {
+      pierwsza = 1;
+      maksimum = liczba;
+      minimum = liczba;
+    }
+    if(liczba>maksimum)
+      maksimum =liczba;
+    if(liczba<minimum)
+      minimum =liczba;
+  }
+  przedzialCalkowity = maksimum-minimum;
+  krokPrzedzialu = przedzialCalkowity/N;
+
+  printf("\nmaksimum: %lf",maksimum);
+  printf("\nminimum: %lf", minimum);
+  printf("\nprzedzialkrokPrzedzialuCalkowity: %lf", przedzialCalkowity);
+  printf("\nkrokPrzedzialu: %lf\n",krokPrzedzialu);
+  rewind(wp);
+  while(fscanf(wp,"%lf\n",&liczba) == 1)
+  {
+    poz = 0;
+    poczatek = minimum;
+    koniec = minimum + krokPrzedzialu;
+    for(i=1;i<=N;i++)
+    {
+        if(poczatek<=liczba && liczba<koniec)
+        {
+          ilosc[poz]++;
+          break;
+        }
+        else
+        {
+          poz++;
+          poczatek = koniec;
+          koniec += krokPrzedzialu;
+        }
+        if(liczba == koniec)
+          ilosc[poz-1]++;
+    }
+  }
+
+  for(i=0.0;i<N;i++)
+  {
+    poz = i;
+    printf("Przedzial %d: %d\n",poz+1,ilosc[poz]);
+  }
+  fclose(wp);
+  system("PAUSE");
+  return 0;
+}
