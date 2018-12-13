78df7f394d153322c0e3347a95621a5d98bf0520
PrzKem <41024254+PrzKem@users.noreply.github.com>
2018-12-13 07:31:59 +0100

    Create 0806.c

---
+++ 0806.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+double potega(double  x, int p)
+{
+  double wynik=1.0;
+  int i;
+  for(i=0;i<p;i++)
+  {
+    wynik *= x;
+  }
+  return(wynik);
+}
+
+
+int main()
+{
+  int p,i;
+  double r;
+  scanf("%d", &p);
+  scanf("%lf", &r);
+  printf("Wynik: %lf\n", potega(r,p));
+
+  system("PAUSE");
+  return(0);
+}
